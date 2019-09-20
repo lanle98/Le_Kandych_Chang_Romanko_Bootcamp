a5547db5040b5dd40af50020c37825f396abe504
Darren Le <darrenle98@gmail.com>
2019-09-20 16:58:37 -0400

    styled form

--- js/submission.js
+++ js/submission.js
@@ -2,6 +2,7 @@
   const send = document.querySelector(".send");
   const back = document.querySelector(".back");
   const submission = document.querySelector(".submission");
+  let body = document.querySelector("body");
 
   //submission function
   send.addEventListener("click", function() {
@@ -9,10 +10,12 @@
     TweenLite.to(window, 0.5, {
       scrollTo: { y: ".submission", offsetY: 20 }
     });
+    body.classList.toggle("overflow");
   });
 
   //back to page function
   back.addEventListener("click", function() {
     submission.classList.toggle("thank-you");
+    body.classList.remove("overflow");
   });
 })();
